73e1eac95696d808980c7327fd46b440a3aa97f0
Craig Fowler <craig@wearegiants.com>
2015-10-27 06:44:34 -0700

    incorporating shopify

---
+++ coterie-3-50132868/snippets/footer.liquid
@@ -0,0 +1,13 @@
+<footer id="footer" class="bg-color-black">
+   <div class="fs-row">
+      <nav class="fs-cell fs-lg-8 fs-md-4 fs-sm-3">
+         <a href="?page=shipping" class="btn btn-nav">shipping/returns</a>
+         <a href="?page=terms" class="btn btn-nav">terms and conditions</a>
+         <a href="?page=contact" class="btn btn-nav">contact</a>
+         <a href="#" class="btn btn-nav">newsletter</a>
+      </nav>
+      <nav class="fs-cell fs-lg-4 fs-md-2 fs-sm-3 text-right">
+         <a href="#" class="btn btn-nav">made by WeAreGiants.</a>
+      </nav>
+   </div>
+</footer>
---
+++ parts/pages/stylesheet.php
@@ -0,0 +1,117 @@
+<div id="styleguide">
+
+  <div class="fs-row">
+    <header class="fs-cell fs-all-full section-header">
+      <h1 class="section-title">Colors</h1>
+    </header>
+  </div>
+
+  <div class="fs-row">
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-black bg"></div>
+      <span>Black (97% Black)</span>
+      <small>#1B1B1C</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-darkgray bg"></div>
+      <span>Dark Grey (50% Black)</span>
+      <small>#939597</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-lightgray bg"></div>
+      <span>Light Grey (10% Black)</span>
+      <small>#E6E7E8</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-warmgray bg"></div>
+      <span>Warm Grey</span>
+      <small>#C9CEC9</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-brightred bg"></div>
+      <span>Bright Red</span>
+      <small>#FF2816</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-softred bg"></div>
+      <span>Soft Red</span>
+      <small>#ED7161</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-lightpink bg"></div>
+      <span>Light Pink</span>
+      <small>#FDE9E3</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-yellow bg"></div>
+      <span>Yellow</span>
+      <small>#FCEF55</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-brightblue bg"></div>
+      <span>Bright Blue</span>
+      <small>#005CA9</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-softblue bg"></div>
+      <span>Soft Blue</span>
+      <small>#6A8CC3</small>
+    </div>
+
+    <div class="fs-cell fs-lg-3 fs-md-fourth fs-sm-third color">
+      <div class="bg-color-softgreen bg"></div>
+      <span>Soft Green</span>
+      <small>#73C48B</small>
+    </div>
+
+  </div>
+
+  <hr class="invisible">
+
+  <div class="fs-row">
+    <header class="fs-cell fs-all-full section-header">
+      <h1 class="section-title">Typography</h1>
+    </header>
+  </div>
+
+  <div class="fs-row">
+    <div class="fs-cell fs-all-full">
+      <h1>Header 1: Maison Neue Light —30pt Size, 50pt Spacing, 36pt Line Height</h1>
+    </div>
+    <div class="fs-cell fs-all-full">
+      <h2>Header 2: Maison Neue Book —18pt Size, 50pt Spacing, 22pt Line Height</h2>
+    </div>
+    <div class="fs-cell fs-all-full">
+      <h3>Header 3: Maison Neue Book —12pt Size, 50pt Spacing, 16pt Line Height</h3>
+    </div>
+    <div class="fs-cell fs-all-full">
+      <h4>HEADER 4: MAISON NEUE MONO —8PT SIZE, 100PT SPACING, 14PT LINE HEIGHT</h4>
+    </div>
+    <div class="fs-cell fs-all-full">
+      <p>
+        This is an example of body copy, to be set in Maison Neue Light. The font size 
+        will be 12 points, while the letter spacing is 50 points. Line height looks good 
+        at around 22 points. Everest et odi volorernam ratem. Erferi aliquist dem en- 
+        tus et eat. Must, commolo riatus earum aborae et moditatur? Omnis moles et litatur, 
+        esto ditios vent alitiunt ulluptas doluptatint fuga. Nam, sit et cones es 
+        sperun tintis doluptatate se et, omnis niam explique.
+      </p>
+  </div>
+
+</div>
+
+
+
+
+
+
