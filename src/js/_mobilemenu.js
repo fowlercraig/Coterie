670fa3504e4a45440fe2aa788af0484ab850a69e
Craig Fowler <craig@wearegiants.com>
2015-11-02 18:42:54 -0800

    things

--- src/js/_mobilemenu.js
+++ src/js/_mobilemenu.js
@@ -1,25 +1,34 @@
 $("#header .btn-hamburg").on('click', function(e){
 
 	e.preventDefault();
-  $('body').css('overflow', 'hidden');
+  $('body,html').css({
+    height: '100%',
+    overflow: 'hidden'
+  });
   $('<div id="header-mobile"><div class="fs-row"><div id="header-mobile--content" class="fs-cell fs-all-full"></div></div></div>').appendTo('#header');
-  $('<a id="header-mobile_close" class="btn-close ss-gizmo ss-delete"></a>').prependTo('#header-mobile');
+  $('<a id="header-mobile_close" class="btn-close btn-nav ss-gizmo ss-delete"></a>').prependTo('#header-mobile--content');
   $('#header .navigation').clone().appendTo('#header-mobile--content').removeClass();
 
   setTimeout(function(){
     $("#header-mobile").addClass('active');
   });
 
+  $("#header-mobile .btn-close").on('click', function(e){
+
+    e.preventDefault();
+    $('body').css('overflow', 'visible');
+    $('#header-mobile').removeClass('active');
+
+    setTimeout(function(){
+      $("#header-mobile").remove();
+    },300);
+
+    $('body,html').css({
+      height: 'auto',
+      overflow: 'visible'
+    });
+
+  });
+
 });
 
-$("#header-mobile .btn-close").on('click', function(e){
-
-	e.preventDefault();
-  $('body').css('overflow', 'visible');
-	$('#header-mobile').removeClass('active');
-
-  setTimeout(function(){
-    $("#header-mobile").remove();
-  },300);
-
-});
