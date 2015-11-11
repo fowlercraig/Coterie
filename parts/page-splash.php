3d3f9fbea6baebe049778a989945e279f744face
Craig Fowler <craig@wearegiants.com>
2015-11-11 08:58:25 -0800

    splash stuff

--- parts/page-splash.php
+++ parts/page-splash.php
@@ -1,18 +1,22 @@
 <div id="splash" class="covered">
-	<div id="splash_centered" class="centered">
-		<div class="content">
-			<div class="fs-row">
-				<div class="fs-cell fs-full-all">
-					<h1 id="splash--title"><?php echo $siteName; ?></h1>
-					<div class="spinner">
-						<div class="bounce1"></div>
-						<div class="bounce2"></div>
-						<div class="bounce3"></div>
-					</div>
-				</div>
+	<div class="content">
+		<div class="page-header">
+			<div class="centered centered-full">
+			  <div class="fs-row">
+			    <div class="fs-cell fs-all-full">
+			      <div class="page-title_wrapper">
+			        <h1 class="page-title"><?php echo $siteName; ?></h1>
+			        <div class="spinner">
+								<div class="bounce1"></div>
+								<div class="bounce2"></div>
+								<div class="bounce3"></div>
+							</div>
+			      </div>
+			    </div>
+			  </div>
 			</div>
 		</div>
+		<div id="splash--overlay"></div>
+		<div id="splash--bg" style="background-image:url('/assets/SnakeOilProvisions_102114_029_blog.jpg');"></div>
 	</div>
-	<div id="splash--overlay"></div>
-	<div id="splash--bg" style="background-image:url('/assets/SnakeOilProvisions_102114_029_blog.jpg');"></div>
 </div>
