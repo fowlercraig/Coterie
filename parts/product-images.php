e94c18ea82b62f592a05d3e1c843774268469475
Craig Fowler <craig@wearegiants.com>
2015-11-11 18:26:09 -0800

    changed images on product page

--- parts/product-images.php
+++ parts/product-images.php
@@ -1,11 +1,11 @@
 <div id="product--images" class="image-gallery carousel" data-carousel-options='{"maxWidth":"980px", "pagination":false}'>
   <figure itemprop="associatedMedia" class="slide">
-    <a itemprop="contentUrl" data-size="800x1200" href="/assets/NOON-GOONS-05.jpg">
+    <a itemprop="contentUrl" data-size="800x1200" href="http://i.imgur.com/BMcdZzX.jpg">
       <img src="/assets/NOON-GOONS-05.jpg" itemprop="thumbnail" class="img-responsive" alt="" />
     </a>
   </figure>
   <figure itemprop="associatedMedia" class="slide">
-    <a itemprop="contentUrl" data-size="800x1200" href="/assets/NOON-GOONS-06.jpg">
+    <a itemprop="contentUrl" data-size="800x1200" href="http://i.imgur.com/mEYBMZK.jpg">
       <img src="/assets/NOON-GOONS-06.jpg" itemprop="thumbnail" class="img-responsive" alt="" />
     </a>
   </figure>
